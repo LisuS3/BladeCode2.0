8262bcf3776bfe30fe268766395371ad48fdc5d3
LisuS3 <64312209+LisuS3@users.noreply.github.com>
2022-12-05 22:11:50 +0100

    added swiper for partners

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
